08688b29735088acf4d35015c0af45d9d3f79745
skokovsv <sskokov007@gmail.com>
2023-04-06 14:40:32 +0300

    7 - UI Bootstrap

--- src/main/java/com/skokov/start/domain/entity/User.java
+++ src/main/java/com/skokov/start/domain/entity/User.java
@@ -22,6 +22,10 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin(){
+        return roles.contains(Role.ADMIN);
+    }
+
     public User(String username,String password,boolean active,Set<Role> roles){
         this.username=username;
         this.password=password;

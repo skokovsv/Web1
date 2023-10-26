0614a9587ee6a986bc9f71f44d8ba94403508f83
skokovsv <sskokov007@gmail.com>
2023-10-26 15:20:07 +0300

    13 - reCaptcha

--- src/main/java/com/skokov/start/configs/MvcConfig.java
+++ src/main/java/com/skokov/start/configs/MvcConfig.java
@@ -2,7 +2,9 @@ package com.skokov.start.configs;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     private String uploadPath;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
--- src/main/java/com/skokov/start/controllers/RegistrationController.java
+++ src/main/java/com/skokov/start/controllers/RegistrationController.java
@@ -1,9 +1,11 @@
 package com.skokov.start.controllers;
 
 
+import com.skokov.start.domain.dto.CaptchaResponseDto;
 import com.skokov.start.domain.entity.User;
 import com.skokov.start.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 
@@ -21,6 +26,12 @@ import java.util.Map;
 public class RegistrationController {
     @Autowired
     private UserService userService;
+
+    @Value("${recaptcha-secret}")
+    private String secret;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/registration")
     public String registration(){
@@ -31,9 +42,20 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(
             @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponse,
+            HttpServletRequest request,
             @Valid User user,
             BindingResult bindingResult,
             Model model){
+        String urlCaptcha = "https://www.google.com/recaptcha/api/siteverify?secret="+
+                secret + "&response=" + captchaResponse;
+        String url = String.format(urlCaptcha,secret,captchaResponse);
+        System.out.println(url);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+        if(!response.isSuccess()){
+            model.addAttribute("messageType","danger");
+            model.addAttribute("message",response.getErrorCodes().toString());
+        }
         boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
         System.out.println(isConfirmEmpty);
         if (isConfirmEmpty){
@@ -43,7 +65,7 @@ public class RegistrationController {
         if (user.getPassword() != null && !user.getPassword().equals( passwordConfirm )){
             model.addAttribute( "message", "Пароли не совпадают!" );
         }
-        if (isConfirmEmpty || bindingResult.hasErrors()){
+        if (isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()){
             Map<String, String> errors = ControllerUtils.getErrors( bindingResult );
             model.mergeAttributes( errors );
             return "registration";
